5a3cd32142fc08688ecce9e9f488e17500427854
Ali Rahbar <alirahbar2005@gmail.com>
2025-03-03 22:52:41 -0500

    Added Main content

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
 
     const handleLogin = async (e) => {
         e.preventDefault()
-        const { error, user } = await db.auth.signIn({
+        const { data, error } = await db.auth.signInWithPassword({
             email,
             password,
         })
